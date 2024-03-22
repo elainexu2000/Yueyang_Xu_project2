4863e79785375afb2e0c14d06189cac80bf08335
elainexu2000 <elainexu2000@gmail.com>
2024-03-22 13:25:02 -0700

    Render

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    outDir: 'build', // Change this to 'build' or 'public' as needed
+  },
+});
